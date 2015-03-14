d68313f140bbfd13371d04ebea2369049294c50e
Pubudu Fernando <pubudu.fernando@gmail.com>
2015-03-15 03:44:28 +0530

    partially done with the timeline

--- symfony/src/FriendsTimelineBundle/Controller/TimelineController.php
+++ symfony/src/FriendsTimelineBundle/Controller/TimelineController.php
@@ -10,16 +10,36 @@ namespace FriendsTimelineBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use FriendsEntityBundle\Entity\Posts;
 
 class TimelineController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $number = rand(1, 10000);
+        $post = new Posts();
 
-        return $this->render(
-            'FriendsTimelineBundle:Timeline:timeline.html.twig',
-            array('number' => $number)
-        );
+        $form = $this->createFormBuilder($post)
+            ->add('content', 'text')
+            ->add('Post', 'submit', array('label' => 'Create Post'))
+            ->getForm();
+
+        return $this->render('FriendsTimelineBundle:Timeline:timeline.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function newAction(Request $request)
+    {
+        // create a post and give it some dummy data for this example
+        $post = new Posts();
+
+        $form = $this->createFormBuilder($post)
+            ->add('post', 'text')
+            ->add('Post', 'submit', array('label' => 'Create Post'))
+            ->getForm();
+
+        return $this->render('FriendsTimelineBundle:Timeline:timeline.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
