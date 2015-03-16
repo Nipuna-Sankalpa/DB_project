7a8586dba72a8414fc3c4502f830da68ebc4a07c
Pubudu Fernando <pubudu.fernando@gmail.com>
2015-03-16 08:16:07 +0530

    new files added

---
+++ symfony/src/FriendsLoginBundle/Controller/SecurityController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pubudu
+ * Date: 3/16/2015
+ * Time: 1:28 AM
+ */
+namespace FriendsLoginBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login2", name="login_route")
+     */
+    public function loginAction(Request $request){
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'FriendsLoginBundle:Login:signin.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+}
